a430bca0c27bac3449641f0761df082992806e81
Brotherta <57185748+Brotherta@users.noreply.github.com>
2019-12-06 17:21:15 +0100

    Update DessinTortue.R

--- DessinTortue.R
+++ DessinTortue.R
@@ -10,7 +10,7 @@ DoIt_Dessin <- function(vecteur,taille_vecteur)  # Fonction principale qui dessi
   print(taille_monde) # debug
   Intitialisation(taille_monde)
   Ciel(taille_monde)
-  #Astre(vecteur,taille_monde)
+  #Astre(taille_monde)
   Chunk(vecteur,taille_vecteur)
   Arbre(vecteur,taille_monde,taille_vecteur)
   Buisson(vecteur,taille_monde,taille_vecteur)
@@ -56,9 +56,9 @@ Arbre <- function(vecteur,taille_monde,taille_vecteur)
   for(i in 1:(taille_vecteur/2 -1))    # un arbre ne peut pas se situer sur le dernier chunk (pour la verification de compteurX + 2)
   {
     compteurY <- compteurY+2
-    if (vecteur[compteurY] > 6)    # On evite de faire un seul if a cause des bugs de TRUE / FALSE needed
+    if (vecteur[compteurY] > 3)    # On evite de faire un seul if a cause des bugs de TRUE / FALSE needed
     {
-      if (vecteur[compteurY+2] > 4 && presence_arbre_position_precedente==0)
+      if (vecteur[compteurY+2] > 2 && presence_arbre_position_precedente==0)
       {
         OutilsArbre( (posX*10 + vecteur[compteurX]*10 *(3/5)) , vecteur[compteurY]*10, taille_monde,vecteur,taille_vecteur)
         presence_arbre_position_precedente<-1
@@ -95,36 +95,6 @@ Buisson <- function(vecteur,taille_monde,taille_vecteur)
     compteurX <- compteurX+2
   }
 }
-
-Astre<-function()
-{
-  turtle_init()
-  turtle_hide()
-  x<-6
-  for(i in 1:60)
-  {
-    turtle_setangle(-20+x)
-    turtle_forward(2)
-    x<-x+6
-  }
-}
-
-Astre2<-function(vecteur,taille_vecteur=40,taille_monde=800)
-{
-  taille<-taille_monde/8  ## ni trop gros ni trop petit. Au pifff
-  if
-  #setpos(taille_monde,(((taille_monde)*3/4)))  
-  for(increment in 360:0)
-  {
-    turtle_setangle(increment)
-    turtle_param(lwd=7)
-    turtle_forward(taille)
-    turtle_backward(taille)
-    
-  }
-}
-
-
   
   
   #Chunk(posX*10, as.integer(SeedVector[compteurY])*10, as.integer(SeedVector[compteurX])*10, as.integer(SeedVector[compteurY])*10)
